375dabf9b38e9498ff82e135e0059765c22b00b1
altbrace <dim203286@gmail.com>
2020-04-25 14:18:16 +0700

    -c option implemented, md5match() function added.

--- src/main.c
+++ src/main.c
@@ -1,67 +1,86 @@
-#include <sys/stat.h>
 #include <dirent.h>
 #include <openssl/md5.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 typedef struct dbEntry {
 	unsigned int id;
-	char* name;
-	char* type;
+	char name[30];
+	char type[10];
 	unsigned int parent_id;
-	unsigned char* md5;
+	unsigned char md5[MD5_DIGEST_LENGTH];
 } dbEntry;
 
 void md5digest(FILE* file, unsigned char* dest) {
 	MD5_CTX md5handler;
-	char data[10];
+	char data;
 	unsigned char md5digest[MD5_DIGEST_LENGTH];
 
 	MD5_Init(&md5handler);
-	while ((fread(data, 1, 1, file) != 0)) {
-		MD5_Update(&md5handler, data, 1);
+	while ((fread(&data, 1, 1, file) != 0)) {
+		MD5_Update(&md5handler, &data, 1);
 	}
 	MD5_Final(md5digest, &md5handler);
 	strcpy((char*)dest, (char*)md5digest);
 }
 
+int md5match(unsigned char* md5_1, unsigned char* md5_2) {
+	if (!md5_1 || !md5_2) {
+		return 1;
+	}
+
+	int i;
+	for (i = 0; i < MD5_DIGEST_LENGTH; i++) {
+		if (md5_1[i] != md5_2[i]) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
 
 int main(int argc, const char** argv) {
-	
 	if (strcmp(argv[1], "-s") == 0 && strcmp(argv[2], "-f") == 0 &&
 	    argc == 5) {
-		FILE* database = fopen(argv[3], "ab+");
+		FILE* database = fopen(argv[3], "wb");
 		DIR* dir = opendir(argv[4]);
 		struct dirent* dirent = readdir(dir);
-		if (dirent == NULL){
-			fprintf(stderr, "Directory %s doesn't exist\n", argv[4]);
+		if (dirent == NULL) {
+			fprintf(stderr, "Directory %s doesn't exist or can't be opened. Check permissions\n",
+				argv[4]);
 			exit(-1);
 		}
 		unsigned int id = 0;
 		dbEntry entry;
-		entry.md5 = malloc(sizeof(char)*MD5_DIGEST_LENGTH);
 		while (dirent != NULL) {
 			if (dirent->d_type == 8) {
 				entry.id = id;
-				entry.name = dirent->d_name;
-				entry.type = "file";
+				strcpy(entry.name, dirent->d_name);
+				strcpy(entry.type, "file");
 				entry.parent_id = 0;
-				const size_t len = strlen(argv[4]) + strlen(entry.name);
-				char* fpath = malloc(sizeof(char)*len);
+				const size_t len =
+				    strlen(argv[4]) + strlen(dirent->d_name);
+				char* fpath = malloc(sizeof(char) * len);
 				strcpy(fpath, argv[4]);
-				strcat(fpath, entry.name);
+				strcat(fpath, dirent->d_name);
 				printf("%s\n", fpath);
 				FILE* file = fopen(fpath, "rb");
-				if (file != NULL){
+				if (file != NULL) {
 					md5digest(file, entry.md5);
 					fclose(file);
-					fwrite(&entry, sizeof(struct dbEntry), 1, database);
-				}
-				else{
-				      printf("Error opening file %s\n", entry.name);
-				      fpath = "";
+					fwrite(&entry,
+					       sizeof(struct dbEntry) +
+						   sizeof(unsigned char) *
+						       MD5_DIGEST_LENGTH +
+						   54,
+					       1, database);
+				} else {
+					printf("Error opening file %s\n",
+					       entry.name);
+					fpath = "";
 				}
 				id++;
 			}
@@ -69,65 +88,66 @@ int main(int argc, const char** argv) {
 		}
 		fclose(database);
 
-	}
-	else if (strcmp(argv[1], "-c") == 0 && strcmp(argv[2], "-f") == 0 && argc == 5){
+	} else if (strcmp(argv[1], "-c") == 0 && strcmp(argv[2], "-f") == 0 &&
+		   argc == 5) {
 		FILE* database = fopen(argv[3], "rb");
-		if (database == NULL){
+		if (database == NULL) {
 			fprintf(stderr, "File %s doesn't exist\n", argv[3]);
 			exit(-1);
 		}
 		DIR* dir = opendir(argv[4]);
-		struct dirent* dirent = readdir(dir);
-		if (dirent == NULL){
-			fprintf(stderr, "Directory %s doesn't exist\n", argv[4]);
+		if (dir == NULL) {
+			fprintf(stderr, "Directory %s doesn't exist\n",
+				argv[4]);
 			exit(-1);
 		}
-		dbEntry entry;
-		entry.md5 = malloc(sizeof(char)*MD5_DIGEST_LENGTH);
-		while (dirent != NULL){
-			if (dirent->d_type == 8){
-				fread(&entry, sizeof(struct dbEntry), 1, database);
-				printf("%s\n", entry.name);
-				if (dirent->d_name == entry.name){
+		struct dirent* dirent = readdir(dir);
+		if (dirent == NULL) {
+			fprintf(stderr, "Directory %s doesn't exist\n",
+				argv[4]);
+			exit(-1);
+		}
+		dbEntry* entry = malloc(sizeof(dbEntry));
+		while (dirent != NULL) {
+			if (dirent->d_type == 8) {
+				fread(entry,
+				      sizeof(struct dbEntry) +
+					  sizeof(unsigned char) *
+					      MD5_DIGEST_LENGTH +
+					  54,
+				      1, database);
+				if (strcmp(dirent->d_name, entry->name) == 0) {
 					unsigned char md5[MD5_DIGEST_LENGTH];
-								
-					const size_t len = strlen(argv[4]) + strlen(entry.name);
-					char* fpath = malloc(sizeof(char)*len);
+					char* name = entry->name;
+					const size_t len =
+					    strlen(argv[4]) + strlen(name);
+					char* fpath =
+					    malloc(sizeof(char) * len);
 					strcpy(fpath, argv[4]);
-					strcat(fpath, entry.name);
+					strcat(fpath, name);
 					printf("%s\n", fpath);
 					FILE* file = fopen(fpath, "rb");
-					if (file != NULL){
+					if (file != NULL) {
 						md5digest(file, md5);
-						if (strcmp((char*)md5, (char*)entry.md5) == 0){
-							printf("File %s hadn't changed\n", entry.name);
-						}
-						else printf("FILE %s CHANGED\n", entry.name);
+						if (md5match(md5, entry->md5) == 0) {
+							printf(
+							    "File %s hadn't "
+							    "changed\n",
+							    entry->name);
+						} else {
+							printf(
+							    "FILE %s CHANGED\n",
+							    entry->name);
+							}	
 					}
+					else printf("Error opening file %s/file is empty\n", entry->name);
+					printf("\n");
 					fpath = "";
 				}
-				
 			}
 			dirent = readdir(dir);
 		}
 	}
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
